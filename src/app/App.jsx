538582a979ec31e9c94e63c0c9f90ca9378383ed
nirajkumar398 <nirajkumar13398@gmail.com>
2024-07-07 16:25:39 +0530

    deployment issue

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Pagination from "./pages/pagination";
 import Home from "./pages/home";
+import InfiniteScroll from "./pages/infinite-scroll";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/machine-coding">
       <Routes>
-        <Route exact path="/machine-coding" element={<Home />} />
-        <Route path="/machine-coding/pagination" element={<Pagination />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/pagination" element={<Pagination />} />
+        <Route path="/infinite-scroll" component={InfiniteScroll} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/app/pages/infinite-scroll/index.jsx
@@ -0,0 +1,5 @@
+const InfiniteScroll = () => {
+  return <div>This site is under construction</div>;
+};
+
+export default InfiniteScroll;
